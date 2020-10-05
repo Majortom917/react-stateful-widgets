c5dee971029694cd49836d3e8ce8b255c62dcbc5
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-05 15:47:32 -0700

    counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -47,14 +47,16 @@ STEP 6:
 */
 
 import React from 'react'; /* STEP 0 */
-
+import React,{useState} from 'react'
 export default function Counter() {
   /* STEP 1 */
-
+const [count, setCount] = useState(0)
   const increment = () => {
+    setCount(count + 1)
     /* STEP 4 */
   };
   const decrement = () => {
+    setCount(count - 1)
     /* STEP 5 */
   };
   const reset = () => {
